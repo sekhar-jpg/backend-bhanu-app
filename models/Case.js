ad65dfc88ba8a60a10ea86a288c37574884a14a1
sekhar-jpg <bhanuhomeohospital@gmail.com>
2025-05-06 22:01:29 +0530

    Update Case.js

--- models/Case.js
+++ models/Case.js
@@ -1,50 +1,20 @@
 const mongoose = require('mongoose');
 
-const caseSchema = new mongoose.Schema({
-  patientName: {
-    type: String,
-    required: true,
-  },
-  phoneNumber: {
-    type: String,
-    required: true,
-  },
-  symptoms: {
-    type: String,
-    required: true,
-  },
-  remedyGiven: {
-    type: String,
-  },
-  followUpDate: {
-    type: Date, // 🔁 This field is used for follow-up tracking
-    required: false,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
-
-module.exports = mongoose.model('Case', caseSchema);
-const mongoose = require('mongoose');
-
 const followUpSchema = new mongoose.Schema({
   date: Date,
   notes: String
-});
+}, { _id: false });
 
 const caseSchema = new mongoose.Schema({
   patientName: String,
   phoneNumber: String,
+  visitDate: {
+    type: Date,
+    default: Date.now
+  },
   symptoms: String,
   mentalSymptoms: String,
-  remedyGiven: String,
-  visitDate: Date,
-  followUps: [followUpSchema], // Array of follow-up objects
-}, {
-  timestamps: true
+  followUps: [followUpSchema]
 });
 
 module.exports = mongoose.model('Case', caseSchema);
-

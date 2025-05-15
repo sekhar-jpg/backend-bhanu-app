402e1383b7a05b36d98baf65a0b205a3b2533a8c
sekhar-jpg <bhanuhomeohospital@gmail.com>
2025-05-15 18:18:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const cors = require('cors'); // Import cors
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Body parser middleware (if needed)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Use cors middleware to enable CORS for your frontend origin
+const corsOptions = {
+  origin: 'https://bhanu-homeo-frontend.onrender.com', // Replace with your frontend URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // If you need to handle cookies or authorization headers
+};
+app.use(cors(corsOptions));
 
 // Import routes
 const rootsRouter = require('./routes/roots');
